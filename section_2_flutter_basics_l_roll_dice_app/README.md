3c23526e6d47e5b529be85bb9f14d537ce6eee66
shreyank.bansod <shreyank.bansod@cionlabs.com>
2024-05-19 14:50:14 +0530

    section_2

---
+++ section_2_flutter_basics_l_roll_dice_app/README.md
@@ -0,0 +1,3 @@
+# section_2_flutter_basics_l_roll_dice_app
+
+A new Flutter project.
---
+++ section_2_flutter_basics_l_roll_dice_app/android/app/src/main/kotlin/com/example/section_2_flutter_basics_l_roll_dice_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.section_2_flutter_basics_l_roll_dice_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
