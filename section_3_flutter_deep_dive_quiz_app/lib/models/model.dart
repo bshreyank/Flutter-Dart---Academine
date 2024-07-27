29339b8ec83b7396c0f0485c628be3cc557fd1ba
shreyank.bansod <shreyank.bansod@cionlabs.com>
2024-07-27 16:27:25 +0530

    Mutating Values in Memory

--- section_3_flutter_deep_dive_quiz_app/lib/models/model.dart
+++ section_3_flutter_deep_dive_quiz_app/lib/models/model.dart
@@ -1,6 +1,12 @@
 class QuizQuestion {
   const QuizQuestion(this.text, this.answers); //Constructor functions
-  
+
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
