68d9abeb17e519e2b8467b38fba621bc0b06ffcb
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2024-07-31 23:00:40 +0530

    section_4_expense_tracker

---
+++ section_4_expense_tracker/README.md
@@ -0,0 +1,3 @@
+# section_4_expense_tracker
+
+A new Flutter project.
---
+++ section_4_expense_tracker/android/app/src/main/kotlin/com/example/section_4_expense_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.section_4_expense_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
